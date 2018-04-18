de978da6fc60ce6ef2715ac404ef7bd8e3b8f101
Ritwik Sharma <ritwik.1204@gmail.com>
2018-04-19 00:03:46 +0530

    Rename analysis.h to .c

--- analysis.c
+++ analysis.c
@@ -1,11 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /* 
- * File:   analysis.h
+ * File:   analysis.c
  *
  * Created on April 10, 2018, 9:56 AM
  */
