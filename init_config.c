1af4042138a2782c8ef7a1334b3d349fe7347894
Ritwik Sharma <ritwik.1204@gmail.com>
2018-04-19 00:08:26 +0530

    Rename init_config.h to .c

--- init_config.c
+++ init_config.c
@@ -1,11 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /* 
- * File:   init_config.h
+ * File:   init_config.c
  *
  * Created on April 10, 2018, 9:18 AM
  */
