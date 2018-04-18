8957e8fe13956dcb6084f80d36135e5cfc9c4a75
Ritwik Sharma <ritwik.1204@gmail.com>
2018-04-19 00:10:38 +0530

    Rename requests.h to .c

--- requests.c
+++ requests.c
@@ -1,11 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /* 
- * File:   requests.h
+ * File:   requests.c
  *
  * Created on April 10, 2018, 8:51 AM
  */
