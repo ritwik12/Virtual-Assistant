4fde7d5042dfb99cbc5823f66a7d15e11eaf2db3
Gaelle Marais <marais.gaelle@gmail.com>
2019-02-14 13:06:18 +0100

    remove useless function

--- email.c
+++ email.c
@@ -30,86 +30,45 @@ int send_email(){
   char title[100]="";
   char body[BUFFER_SIZE]="";
 
+  //Asking infos to user (to, title,body)
+  //TODO
+
   //calling email2 on another processus
   const pid_t pid = fork();
   if(pid == 0) {
     char *arg[] = { "./smtp", (char *) 0 };
     execvp( "./smtp", arg);
-   }else{
+  }else{
     waitpid(pid, NULL, 0);
-   }
-
-   printf("OK \n");
-
+  }
   return 0;
 
 }
 
 int email(){
-
   //searching for config_email, create it if it does not exist
   FILE* config_email = NULL;
-    config_email = fopen("config_email", "r+");
-    if(config_email==NULL){
-      config_email_function();
-    }
+  config_email = fopen("config_email", "r+");
+  if(config_email==NULL){
+    config_email_function();
+  }
 
-
-    printf("What do you want to do with your emails ? (read/send)\n");
-    char action[20]="";
-    scanf("%s", action);
-    if(strcmp(action,"send")==0 || strcmp(action,"sned")==0 ){
-        //  printf("SENDING EMAIL : NOT IMPLEMENTED YET\n");
-        send_email();
-    }else if(strcmp(action,"read")==0 || strcmp(action,"raed")==0){
-        printf("READING EMAIL : NOT IMPLEMENTED YET\n");
-    } else{
-      printf("Sorry I can't do that.\n\n");
-    }
+  //Asking action to user
+  printf("What do you want to do with your emails ? (read/send)\n");
+  char action[20]="";
+  scanf("%s", action);
+  if(strcmp(action,"send")==0 || strcmp(action,"sned")==0 ){
+    send_email();
+  }else if(strcmp(action,"read")==0 || strcmp(action,"raed")==0){
+    printf("READING EMAIL : NOT IMPLEMENTED YET\n");
+    //TODO
+  } else{
+    printf("Sorry I can't do that.\n\n");
+  }
 }
 
 
 
 int read_email(){
   //TODO
-}
-
-
-
-void SMTPrequest(int sock,char *req,char *from,char *to,char *title,char *body){
-  //resp contains the response from SMTP protocol
-  char resp[100]="";
-
-  //wrinting informations in req using SMTP format and send it to the socket
-  sprintf(req, "HELO localhost\r\n");
-  send(sock,req, strlen(req),0);
-  recv(sock, resp, strlen(resp), 0);
-
-  sprintf(req, "AUTH PLAIN AG1hbmdhYm91bnR5bmFpYUBnbWFpbC5jb20ATUFOR0FCT1VOVFlOQUlB\r\n");
-  send(sock,req, strlen(req),0);
-  recv(sock, resp, strlen(resp), 0);
-
-  sprintf(req, "MAIL FROM: <%s>", from);
-  send(sock, req, strlen(req), 0);
-  recv(sock, resp, strlen(resp), 0);
-
-  sprintf(req, "RCPT TO: <%s>", to);
-  send(sock, req, strlen(req), 0);
-  recv(sock, resp, strlen(resp), 0);
-
-  sprintf(req, "DATA\r\n");
-  send(sock, req, strlen(req), 0);
-  recv(sock, resp, strlen(resp), 0);
-
-  sprintf(req, "Subject: <%s> \r\n%s\r\n.\r\n", title, body);
-  send(sock, req, strlen(req), 0);
-  recv(sock, resp, strlen(resp), 0);
-
-  //Print response for tests
-  printf("%s",resp);
-
-  //Close SMTP
-  sprintf(req, "QUIT\r\n");
-  send(sock, req, strlen(req), 0);
-  recv(sock, resp, strlen(resp), 0);
 }
